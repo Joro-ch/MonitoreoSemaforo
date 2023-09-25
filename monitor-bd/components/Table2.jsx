27096195b2215e3b0d6e1683aa43eea0609e57f6
Albert14Aguero <albert14aguero@gmail.com>
2023-09-24 18:26:01 -0600

    Finalizar segunda tabla

--- monitor-bd/components/Table2.jsx
+++ monitor-bd/components/Table2.jsx
@@ -1,8 +1,35 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Table2() {
 
+    const [item, setItem] = useState([]);
+    useEffect(() => {
+        charge();
+    }, []);
+
+    const charge = async () =>{
+
+
+        try{const response = await fetch(`/api/sentenciasInfo`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        if (!response.ok) {
+            alert('Error al obtener la capacidad de los tablespaces')
+        }
+        else {
+            const data = await response.json()
+            setItem(data.infoSentencias)
+            
+        }
+    }catch (error) {
+        console.error("Error al obtener la capacidad de los tablespaces", error);
+    }   
+    };
 
     return(
         <table>
@@ -10,19 +37,23 @@ export default function Table2() {
                 <tr>
                 <th scope="col">Fecha</th>
                 <th scope="col">Hora</th>
-                <th scope="col">cod</th>
+                <th scope="col">Consumo</th>
                 <th scope="col">Id</th>
                 <th scope="col">Sql</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>asdas</td>
-                    <td>asd</td>
-                    <td>asd</td>
-                    <td>asd</td>
-                    <td>asd</td>
-                </tr>
+                {item.map((e,i) =>(
+                        <tr key={i}>
+                            <td scope="row">{e.Fecha}</td>
+                            <td>{e.Hora}</td>
+                            <td>{e.SESSION_CPU}</td>
+                            <td>{e.SESSIONID}</td>
+                            <td>{e.ACTION_NAME}</td>
+                        </tr>
+                    )
+                    )}
+                
             </tbody>
         </table>
     );
---
+++ monitor-bd/pages/api/sentenciasInfo.js
@@ -0,0 +1,42 @@
+const oracledb = require('oracledb');
+const dbConfig = require('./configBD');
+
+export default async (req, res) => {
+    try {
+      // Establecer una conexión a la base de datos Oracle
+      const connection = await oracledb.getConnection(dbConfig);
+  
+      // Usar AUDIT ALL; por cualquier cosa
+
+      const query = `
+        SELECT  TO_CHAR(timestamp, 'YYYY-MM-DD'),
+        TO_CHAR(timestamp, 'HH24:MI:SS'),
+        SESSION_CPU,
+        SESSIONID, 
+        ACTION_NAME
+        FROM DBA_AUDIT_TRAIL
+        where action_name IN ('SELECT', 'INSERT', 'UPDATE', 'DELETE')
+      `;
+      const result = await connection.execute(query);
+  
+      // Cerrar la conexión
+      await connection.close();
+  
+      const infoSentencias = result.rows.map(row => {
+        return {
+            Fecha: row[0],
+            Hora: row[1],
+            SESSION_CPU: row[2]?row[2]:0,
+            SESSIONID: row[3],
+            ACTION_NAME: row[4],
+
+        };
+      });
+  
+      // Enviar la lista de tablespaces como respuesta
+      res.status(200).json({ infoSentencias });
+    } catch (error) {
+      console.error('Error al obtener la capacidad de los tablespaces', error);
+      res.status(500).json({ error: 'Error al obtener la capacidad de los tablespaces' });
+    }
+  };
